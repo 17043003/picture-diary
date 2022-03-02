0405dfb15f1b0a0169ebb2673210827965e1d07a
17043003 <ishizuka533725@gmail.com>
2022-03-02 22:07:24 +0900

    add get post list.

--- src/main/kotlin/com/ishzk/model/Post.kt
+++ src/main/kotlin/com/ishzk/model/Post.kt
@@ -1,10 +1,19 @@
 package com.ishzk.model
 
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.ResultRow
 
 object PostsTable: IntIdTable("posts") {
     val title = varchar("title", 255).nullable()
     val body = varchar("body", 65535).nullable()
+
+    fun toPost(row: ResultRow): Post {
+        return Post(
+            id = row[id].value.toLong(),
+            title = row[title]!!,
+            body = row[body]!!
+        )
+    }
 }
 
 object ImagesTable: IntIdTable("images") {
--- src/main/kotlin/com/ishzk/repository/PostRepository.kt
+++ src/main/kotlin/com/ishzk/repository/PostRepository.kt
@@ -1,8 +1,10 @@
 package com.ishzk.repository
 
+import com.ishzk.model.Post
 import com.ishzk.model.PostRequest
 import com.ishzk.model.PostsTable
 import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class PostRepository {
@@ -12,6 +14,14 @@ class PostRepository {
                 it[title] = request.title
                 it[body] = request.body
             }
+        }
+    }
+
+    fun getPosts(): List<Post> {
+        return transaction {
+            PostsTable.selectAll()
+        }.map {
+            PostsTable.toPost(it)
         }
     }
 }
