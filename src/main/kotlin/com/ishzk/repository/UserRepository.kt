b71d66be5ba9ed03b19bfefd5858381ce81d8efe
17043003 <ishizuka533725@gmail.com>
2022-03-11 01:06:42 +0900

    add getUser by email.

--- src/main/kotlin/com/ishzk/repository/UserRepository.kt
+++ src/main/kotlin/com/ishzk/repository/UserRepository.kt
@@ -1,8 +1,11 @@
 package com.ishzk.repository
 
+import com.ishzk.model.User
 import com.ishzk.model.UserRequest
 import com.ishzk.model.UsersTable
+import com.ishzk.model.UsersTable.toUser
 import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class UserRepository {
@@ -12,6 +15,14 @@ class UserRepository {
                 it[name] = request.name
                 it[email] = request.email
             }[UsersTable.id].value
+        }
+    }
+
+    fun getUser(email: String): User {
+        return transaction {
+            UsersTable.select {
+                UsersTable.email eq email
+            }.map { toUser(it) }.single()
         }
     }
 }
